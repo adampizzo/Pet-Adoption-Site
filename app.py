ffde72a5c3990749b4b251519b6bd6aae11dee96
Adam Pizzo <50724722+adampizzo@users.noreply.github.com>
2022-04-20 19:12:33 -0400

    Worked through course
    added SQL through SQLAlchemy (flask-sqlalchemy). Added and updated page for adding pets, deleting pets, and copying pets.
    
    Added 404 Error page.

--- app.py
+++ app.py
@@ -1,25 +1,84 @@
-from flask import (Flask, render_template, url_for, request)
-import sqlalchemy
-
-
-app = Flask(__name__)
+from flask import (redirect, render_template, url_for, request)
+from models import db, Pet, app
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    pets = Pet.query.all()
+    return render_template('index.html', pets=pets)
 
 
-@app.route('/add-pet', methods=('Get', 'POST'))
+@app.route('/add-pet', methods=['GET', 'POST'])
 def add_pet():
-    print(request.form)
+    if request.form:
+        print(request.form)
+        print(request.form['name'])
+        new_pet = Pet(name=request.form['name'], age=request.form['age'],
+                    breed=request.form['breed'], color=request.form['color'],
+                    size=request.form['size'], weight=request.form['weight'],
+                    url=request.form['url'], url_tag=request.form['alt'],
+                    pet_type=request.form['pet'], gender=request.form['gender'],
+                    spay=request.form['spay'], house_trained=request.form['housetrained'],
+                    description=request.form['description'])
+        db.session.add(new_pet)
+        db.session.commit()
+        return redirect(url_for('index'))
     return render_template('addpet.html')
 
 
-@app.route('/pet')
-def pet():
-    return render_template('pet.html')
+@app.route('/pet/<id>')
+def pet(id):
+    pet = Pet.query.get_or_404(id)
+    return render_template('pet.html', pet=pet)
+
+
+@app.route('/edit-pet/<id>', methods=['GET', 'POST'])
+def edit_pet(id):
+    pet = Pet.query.get_or_404(id)
+    
+    if request.form:
+        print(request.form)
+        print(request.form['name'])
+        pet.name = request.form['name'] 
+        pet.age = request.form['age']
+        pet.breed = request.form['breed'] 
+        pet.color = request.form['color']
+        pet.size = request.form['size'] 
+        pet.weight = request.form['weight']
+        pet.url = request.form['url']
+        pet.url_tag = request.form['alt']
+        pet.pet_type = request.form['pet']
+        pet.gender = request.form['gender']
+        pet.spay = request.form['spay'] 
+        pet.house_trained = request.form['housetrained']
+        pet.description = request.form['description']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('editpet.html', pet=pet)
+
+
+@app.route('/delete-pet/<id>', methods=['GET', 'POST'])
+def delete_pet(id):
+    pet = Pet.query.get_or_404(id)
+    db.session.delete(pet)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/copy-pet/<id>', methods=['GET', 'POST'])
+def copy_pet(id):
+    pet = Pet.query.get_or_404(id)
+    new_pet = Pet.clone(pet)
+    db.session.add(new_pet)
+    db.session.commit()
+    return redirect(url_for('edit_pet', id=new_pet.id))
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', msg=error), 404
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True, port=8000, host='localhost')
